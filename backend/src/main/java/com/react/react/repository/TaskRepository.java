8cdca8d533e4a19b9c65c8ba4cc0d24f87132a4f
wiliam tae <wiliam.tae@gmail.com>
2022-06-05 09:04:25 -0300

    feat: basic structure

---
+++ backend/src/main/java/com/react/react/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.react.react.repository;
+
+import com.react.react.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    public Task findByName(String name);
+}
